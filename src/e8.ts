121cf5864cc144e3d910ebc48ae61449dd3612a8
Nartallax <nartallax@gmail.com>
2024-05-06 08:25:58 +0200

    Post-split updates for easier development

--- src/e8.ts
+++ src/e8.ts
@@ -4,8 +4,12 @@ import {ResourcePackDecoder} from "resource_pack/resource_pack_decoder"
 import {ResourcePackEncoder} from "resource_pack/resource_pack_encoder"
 import type {Engine, EngineLoader} from "types"
 
+export {knownMouseButtonInputs} from "user_input/inputs"
+export {chordToString, fixChord} from "user_input/keys/input_key_utils"
+
 export type {Engine, XY} from "types"
 export type {ResourcePack} from "resource_pack/resource_pack"
+export {EntityImpl as Entity} from "glue/entity"
 
 export const makeE8Loader = <T extends Engine>(): EngineLoader<T> => new EngineLoaderImpl<T>()
 export const encodeResourcePack = (resourcePack: ResourcePack): Uint8Array => new ResourcePackEncoder(resourcePack).encode()
